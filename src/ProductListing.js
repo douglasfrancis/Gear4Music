43396afdac5793b7c4f71e4e46ebfc9d3bc86bad
Douglas Francis <dougiefrancis@gmail.com>
2020-06-13 14:44:39 +0100

    Product Listing: Product availability added

--- src/ProductListing.js
+++ src/ProductListing.js
@@ -3,15 +3,25 @@ import './index.css'
 
 
 const ProductListing = (props) => {
-  
+  const qty = (availability) => {
+    if(availability > 20) {
+      return '20+ in stock'
+    } else if(availability < 5){
+      return "Less than 5 left, order yours now"
+    } else {
+      return availability + ' available'
+    }
+  }
   return (
-    <a href={`'/products/${props.code}'`}>
+    <a href={`/products/${props.code}`}>
       <div className='box'>
 
       <div>{props.title}</div>
-      <div><img width= '50' height= 'auto' src={props.image}/></div>
-      <div>{props.availability}</div>
-      <div>£{props.price}</div>
+      <div><img width= '100%' height= 'auto' src={props.image}/></div>
+      <div className='qty'>{qty(props.availability)}</div>
+
+  
+      <div className='price'>£{props.price}</div>
       <div className='button'><button onClick= {props.addToBasket}>Add To Basket</button></div>
 
     </div>
