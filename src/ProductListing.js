6428429dc11f5db5ef47da67599403701b5fa386
Douglas Francis <dougiefrancis@gmail.com>
2020-06-13 11:12:57 +0100

    Index.js: Base App created with props with template props

--- src/ProductListing.js
+++ src/ProductListing.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import './index.css'
+import image from './Guitar.jpg'
 
 const ProductListing = (props) => {
   
   return (
-    <div className='box'>
+    <a href='/products/{code}'>
+      <div className='box'>
 
-      <div>title {props.title}</div>
-      <div><img src={props.image}/></div>
-      <div>availability {props.availability}</div>
-      <div>price {props.price}</div>
+      <div>{props.title}</div>
+      <div><img src={image}/></div>
+      <div>{props.availability}</div>
+      <div>{props.price}</div>
       <div className='button'><button onClick= {props.addToBasket}>Add To Basket</button></div>
 
     </div>
+    </a>
   )
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductListing from './ProductListing';
 
@@ -12,8 +13,25 @@ const init = () => {
   const listingsPage = document.getElementById('container');
 
   if (listingsPage) {
-      ReactDOM.render(<ProductListing addToBasket={addToBasket} />, listingsPage);
+    return <ProductListing addToBasket={addToBasket} />
   }
 };
+
+function App() {
+
+  return <div className='products'>
+            <ProductListing addToBasket={addToBasket} title='Guitar 1' image='Guitar.jpg' availability='5 available' price='£550' />
+            <ProductListing addToBasket={addToBasket} title='Guitar 2' image='Guitar.jpg' availability='5 available' price='£550' />
+            <ProductListing addToBasket={addToBasket} title='Guitar 3' image='Guitar.jpg' availability='5 available' price='£550' />
+            <ProductListing addToBasket={addToBasket} title='Guitar 4' image='Guitar.jpg' availability='5 available' price='£550' />
+            <ProductListing addToBasket={addToBasket} title='Guitar 5' image='Guitar.jpg' availability='5 available' price='£550' />
+            <ProductListing addToBasket={addToBasket} title='Guitar 6' image='Guitar.jpg' availability='5 available' price='£550' />
+            <ProductListing addToBasket={addToBasket} title='Guitar 7' image='Guitar.jpg' availability='5 available' price='£550' />
+            <ProductListing addToBasket={addToBasket} title='Guitar 8' image='Guitar.jpg' availability='5 available' price='£550' />
+
+          </div>
+
+};
+ReactDOM.render(<App />, document.getElementById('container'));
 
 document.addEventListener('DOMContentLoaded', init);
